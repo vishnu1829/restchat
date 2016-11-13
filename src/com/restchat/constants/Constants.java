d1ec2870ef5db591efcaf84487d48716ea07eed2
Satheesh Maddireddi <Satheesh Maddireddi>
2016-11-12 19:15:48 -0600

    changes related to testing git

--- src/com/restchat/constants/Constants.java
+++ src/com/restchat/constants/Constants.java
@@ -3,7 +3,7 @@ package com.restchat.constants;
 public interface Constants {
 	public String senderId="senderId";
 	public String currentTimeMillis="ct";
-	public String message="message";
+	public String message="vishnu";
 	
 
 }
